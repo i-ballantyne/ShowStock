2564f4a4e7eece842315a642c048e4846d0b216a
Iain Ballantyne <iain.ballantyne@keemail.me>
2019-07-24 15:46:20 +0100

    Initial Commit

---
+++ IainBallantyne/ShowStock/view/frontend/requirejs-config.js
@@ -0,0 +1,9 @@
+var config = {
+    config: {
+        mixins: {
+            'Magento_ConfigurableProduct/js/configurable': {
+                'IainBallantyne_ShowStock/js/configurable': true
+            }
+        }
+    }
+};
